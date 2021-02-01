e8fc9b59f70e2d2e7a50df32302773d29ec94984
ljl <776745393@qq.com>
2021-02-01 18:01:53 +0800

    materialapp，scaffold，appbar，container，text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -10,11 +10,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text(
-        '你好Flutter',
-        textDirection: TextDirection.ltr,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App bar'),
+        ),
+        body: HomeContent(),
       ),
+      theme: ThemeData(
+        primaryColor: Colors.amberAccent,
+      ),
+    );
+  }
+}
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: Container(
+        child: Image.network("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=125666714,702516135&fm=26&gp=0.jpg"),
+        height: 300,
+        width: 300,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.red
+            )
+          )
+        ),
+      );
+  }
+}
+/// 设置home的body内容(container，text)
+class HomeContent1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Text('这是一个',
+        // textAlign: TextAlign.left,
+        style: TextStyle(
+          color: Color.fromRGBO(2, 22, 222, 0.9),
+          fontWeight: FontWeight.w700,
+          letterSpacing: 3,
+          decoration: TextDecoration.lineThrough,
+          decorationColor: Colors.blue,
+          decorationStyle: TextDecorationStyle.double
+        ),
+          textScaleFactor: 1,
+          // overflow: TextOverflow.ellipsis,
+          maxLines: 3,
+
+        ),
+        // padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+        height: 1000,
+        width: 300,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.deepOrange,
+            width: 3,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(5)),
+        ),
+        transform: Matrix4.diagonal3Values(1.3, 1, 1),
+        alignment: Alignment.centerRight,
+      )
     );
   }
 }
